6593d32f61fcef7e437a0623b60a97d2083b49b0
veasnapovsr <veasnavarapov@gmail.com>
2025-11-02 11:03:05 +0700

    Update test.js

--- test/test.js
+++ test/test.js
@@ -8,6 +8,7 @@ describe('GET /', () => {
   it('should return "Hello, GitHub Actions!"', async () => {
     const res = await request(app).get('/');
     res.status.should.equal(200);
-    res.text.should.equal('Hello, GitHub Actions!');
+    res.text.should.equal('Hello CI/CD!');
   });
+
 });
